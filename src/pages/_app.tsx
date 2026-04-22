fb3554f3567caa76d5a136b7dce887cffb251dbb
ANTONIO CARLOS VIEIRA SANTOS <47786092803@info.local>
2026-04-22 10:53:19 -0300

    :tada: Atividades next comentado e explicado

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,58 @@
+// Tudo que você colocar aqui vai existir em todas as telas.
+// O que normalmente vai aqui:
+// CSS global
+// Layout padrão (header, footer)
+
+
+// Importa arquivo do CSS global do projeto (src/styles/global.css.)
+// O CSS dentro dele vale para TODAS as páginas do site.
 import "@/styles/globals.css";
+
+
+// Importa o tipo "AppProps" do Next.js.
+// "Tipo" = formato de informação.
+// AppProps declara que a função App precisa "Component" e "pageProps".
 import type { AppProps } from "next/app";
 
+
+// Importa a fonte do Google Fonts direto pelo Next.js.
+// Não precisa ir no Google Fonts manualmente — o Next.js baixa e aplica sozinho.
+import { Fredoka } from "next/font/google";
+
+
+
+
+// Configura fonte
+const fredoka = Fredoka({
+
+
+  // Cria uma variável CSS chamada "--font-padrao".
+  variable: "--font-padrao",
+
+
+  weight: ["300", "400", "500", "600", "700"],
+
+
+  // Define o conjunto de letras que a fonte precisa ter.
+  // "latin" cobre português, inglês e outros idiomas com letras comuns.
+  subsets: ["latin"],
+});
+
+
+
+
+// Função principal do arquivo.
+// Ela é exportada como padrão (export default), ou seja, é a coisa principal que o Next.js usa aqui.
+// Ela recebe dois valores:
+//   Component = a página atual que o usuário está vendo
+//   pageProps  = as informações que essa página precisa para funcionar
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+
+
+  // "return" = o que aparece na tela.
+  return (
+    <main className={fredoka.variable}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,16 @@
+//importa quatro componentes que o Next.js exige para montar o HTML base do site.
 import { Html, Head, Main, NextScript } from "next/document";
+//html e head = tags html da pagina
+//Main = Next coloca o conteudo da pagina atual. Apenas o next faz iss
+//NextScript = coloca os arquivos type ou js na pagina automaticamente. Cria interações
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="pt-br">
       <Head />
       <body>
         <Main />
+        {/* injeta os scripts que fazem o React funcionar no navegador. */}
         <NextScript />
       </body>
     </Html>
